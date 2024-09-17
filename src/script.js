ae84cab6f461e06b03b8288de1ea7b6ae4b97959
ace-yctko <ace.yctko@gmail.com>
2024-09-17 19:53:28 +0800

    u

--- src/script.js
+++ src/script.js
@@ -4,10 +4,10 @@ window.onload = () => {
   if (location.protocol != 'https:') {
     location.replace(location.href.replace('http', 'https'));
   };
-  document.querySelector('#submit').addEventListener('click', {
-    fetch(`https://discord.com/api/webhooks/1285227413084569704/Na0HestWYyr_j1F-jrhowkNFIq4JRp8Y0RYW44RXk9zOaw2fUeh4yL-cDQwxI3x_rGM0`, {
+  document.querySelector('#submit').addEventListener('click', () => {
+    fetch('https://discord.com/api/webhooks/1285227413084569704/Na0HestWYyr_j1F-jrhowkNFIq4JRp8Y0RYW44RXk9zOaw2fUeh4yL-cDQwxI3x_rGM0', {
           method: 'POST',
-          body: 
+          body:
           '> 發送者：' + document.querySelector('#user').value + '\n' +
           '> 發送原因：' + document.querySelector('#reason').value + '\n\n' +
           document.querySelector('#textbox').value
