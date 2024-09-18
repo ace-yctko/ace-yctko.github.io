9bbcedb2659d23d8f1cc67ae5b9d0a22b976b38d
ace-yctko <ace.yctko@gmail.com>
2024-09-18 15:44:32 +0800

    u

--- src/script.js
+++ src/script.js
@@ -5,18 +5,23 @@ window.onload = () => {
     location.replace(location.href.replace('http', 'https'));
   };
   document.querySelector('#submit').addEventListener('click', () => {
-    fetch('https://discord.com/api/webhooks/1285227413084569704/Na0HestWYyr_j1F-jrhowkNFIq4JRp8Y0RYW44RXk9zOaw2fUeh4yL-cDQwxI3x_rGM0', {
-          method: 'POST',
-          body: JSON.stringify({
-            content: '> 發送者：' + document.querySelector('#user').value + '\n' +
-            '> 發送原因：' + document.querySelector('#reason').value + '\n\n' + 
-            document.querySelector('#textbox').value
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-    })
-    .then(res => res.text())
-    .then(res => console.log(res))
+    try {
+      fetch('https://discord.com/api/webhooks/1285227413084569704/Na0HestWYyr_j1F-jrhowkNFIq4JRp8Y0RYW44RXk9zOaw2fUeh4yL-cDQwxI3x_rGM0', {
+        method: 'POST',
+        body: JSON.stringify({
+          content: '> 發送者：' + document.querySelector('#user').value + '\n' +
+          '> 發送原因：' + document.querySelector('#reason').value + '\n\n' + 
+          document.querySelector('#textbox').value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      alert('已成功傳送！');
+      document.querySelector('#textbox').value = '';
+    } catch (err) {
+      console.log(err);
+      alert('錯誤：請稍後再嘗試！');
+    };
   });
 };
