ea016b8aaf22ed1a2faa31070684473298e5d175
ace-yctko <ace.yctko@gmail.com>
2024-09-18 14:54:03 +0800

    u

--- src/script.js
+++ src/script.js
@@ -7,7 +7,9 @@ window.onload = () => {
   document.querySelector('#submit').addEventListener('click', () => {
     fetch('https://discord.com/api/webhooks/1285227413084569704/Na0HestWYyr_j1F-jrhowkNFIq4JRp8Y0RYW44RXk9zOaw2fUeh4yL-cDQwxI3x_rGM0', {
           method: 'POST',
-          body: `> 發送者： ${document.querySelector('#user').value}\n> 發送原因： ${document.querySelector('#reason').value}\n\n${document.querySelector('#textbox').value}`,
+          body: '{content: "> 發送者：' + document.querySelector('#user').value + '\n' +
+          '> 發送原因：' + document.querySelector('#reason').value + '\n\n' + 
+          document.querySelector('#textbox').value + '"}',
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
           }
