0571dfadda215c182835717c9f97ceab7da69396
ace-yctko <ace.yctko@gmail.com>
2024-10-03 16:39:02 +0800

    Update shops-benefit.js

--- src/shops-benefit.js
+++ src/shops-benefit.js
@@ -1,16 +1,20 @@
 'use strict';
 
-let id = '1gTkcU8G4240QNNcMdFWTwr4JKBBd1Qdjwpu8nE8gY7E',
-    title = 'shops benefit',
-    range = 'A:I';
+const list = (title) => {
+    let id = '1gTkcU8G4240QNNcMdFWTwr4JKBBd1Qdjwpu8nE8gY7E',
+    title = title,
+    range = 'A:I'; 
 
-let url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?sheet=' + title + '&range=' + range;
+    let url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?sheet=' + title + '&range=' + range;
 
-window.onload = () => {
     fetch(url)
     .then(res => res.text())
     .then(rep => {
         let data = JSON.parse(rep.substr(47).slice(0, -2));
-        console.log(data);
+        return data;
     });
+};
+
+window.onload = () => {
+    console.log(list('食肆/食品零售'));
 };
