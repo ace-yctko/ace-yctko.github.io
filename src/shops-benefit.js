54665c03f04065c1fae51b00f7a6ca9254104713
ace-yctko <ace.yctko@gmail.com>
2024-10-14 20:48:57 +0800

    u

--- src/shops-benefit.js
+++ src/shops-benefit.js
@@ -12,7 +12,7 @@ const online_list = (title) => {
         let data = JSON.parse(rep.substr(47).slice(0, -2)).table.rows;
 
         document.querySelector('.online_shop').innerHTML =
-        '<tr><th>獨家</th><th>IG/店舖名</th><th>聯絡方式</th><th>地址</th><th>福利</th><th>適用時段</th><th>備注</th></tr>';
+        '<tr><th>獨家</th><th>IG/店舖名</th><th>聯絡方式</th><th>福利</th><th>適用時段</th><th>備注</th></tr>';
 
         for (let i in data) {
             if (data[i].c[0].v == '❌'|| data[i].c[4].v != 'N/A') continue;
