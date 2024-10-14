f6743bccf516f62ae5e9602c9f86c50ad5af19e1
ace-yctko <ace.yctko@gmail.com>
2024-10-14 20:49:44 +0800

    u

--- src/shops-benefit.js
+++ src/shops-benefit.js
@@ -2,7 +2,7 @@
 
 const online_list = (title) => {
     let id = '1gTkcU8G4240QNNcMdFWTwr4JKBBd1Qdjwpu8nE8gY7E',
-    range = 'A3:I'; 
+    range = 'A3:H'; 
 
     let url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?sheet=' + title + '&range=' + range;
 
