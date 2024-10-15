b72744649a58d81e4350f008b3c3f3932963b810
ace-yctko <ace.yctko@gmail.com>
2024-10-15 13:07:36 +0800

    u

--- src/shops-benefit.js
+++ src/shops-benefit.js
@@ -15,7 +15,7 @@ const list = () => {
 
             for (let i in data) {
                 if (data[i].c[0].v == '❌') continue;
-                temp += `<tr class="${title.id} style="display:none;">`;
+                temp += `<tr class="${title.id}" style="display:none;">`;
                 for (let j in data[i].c) {
                     if (j == 2 && (data[i].c[3].v).indexOf('IG') != -1) {
                         temp += '<td>' + `<iframe src="https://www.instagram.com/${data[i].c[j]?.v}/embed" scrolling="no" frameborder="0"></iframe>` + '</td>';
