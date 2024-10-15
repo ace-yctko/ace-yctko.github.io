8a904ef0573f5ec194a6c137c487735b7c27e00f
ace-yctko <ace.yctko@gmail.com>
2024-10-15 12:27:30 +0800

    u

--- src/shops-benefit.js
+++ src/shops-benefit.js
@@ -1,30 +1,30 @@
 'use strict';
 
-const list = async () => {
+const list = () => {
     const id = '1gTkcU8G4240QNNcMdFWTwr4JKBBd1Qdjwpu8nE8gY7E',
     range = 'A3:H'; 
 
     let temp = '';
 
-    document.querySelectorAll('#type > option').forEach(title => {
+    document.querySelectorAll('#type > option').forEach(async title => {
         let url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?sheet=' + title.id + '&range=' + range;
 
-        fetch(url)
+        await fetch(url)
         .then(res => res.text())
         .then(rep => {
             let data = JSON.parse(rep.substr(47).slice(0, -2)).table.rows;
 
             for (let i in data) {
                 if (data[i].c[0].v == '❌') continue;
-                await temp += `<tr class="${title.id} disable">`;
+                temp += `<tr class="${title.id} disable">`;
                 for (let j in data[i].c) {
                     if (j == 2 && (data[i].c[3].v).indexOf('IG') != -1) {
-                        await temp += '<td>' + `<iframe src="https://www.instagram.com/${data[i].c[j]?.v}/embed" scrolling="no" frameborder="0"></iframe>` + '</td>';
+                        temp += '<td>' + `<iframe src="https://www.instagram.com/${data[i].c[j]?.v}/embed" scrolling="no" frameborder="0"></iframe>` + '</td>';
                     } else if (j == 2 || j > 4) {
-                        await temp += '<td>' + data[i].c[j]?.v || 'N/A' + '</td>';
+                        temp += '<td>' + data[i].c[j]?.v || 'N/A' + '</td>';
                     };
                 };
-                await temp += '</tr>';
+                temp += '</tr>';
             };
         });
     });
