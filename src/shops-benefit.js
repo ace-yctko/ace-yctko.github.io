5049626cb10cdbe93d4af241255c7ac5cf06bafc
ace-yctko <ace.yctko@gmail.com>
2024-10-16 18:23:06 +0800

    u

--- src/shops-benefit.js
+++ src/shops-benefit.js
@@ -1,21 +1,33 @@
 'use strict';
 
-const list = () => {
+const filter = (title, prev) => {
+    document.querySelectorAll(`.${title}`).forEach(x => {
+        x.removeAttribute('style');
+    });
+    if (prev) document.querySelectorAll(`.${prev}`).forEach(x => {
+        x.style.display = 'none';
+    });
+};
+
+window.onload = () => {
     const id = '1gTkcU8G4240QNNcMdFWTwr4JKBBd1Qdjwpu8nE8gY7E',
-    range = 'A3:H'; 
-
+        range = 'A3:H';
+    let type = localStorage.type || document.querySelector('#type > option').id;
+    
     document.querySelectorAll('#type > option').forEach(title => {
-        let url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?sheet=' + title.id + '&range=' + range;
+        title.innerHTML = title.id;
+        title.value = title.id;
+        document.querySelector(`#${type}`).selected = true;
 
-        fetch(url)
+        fetch('https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?sheet=' + title.id + '&range=' + range)
         .then(res => res.text())
         .then(rep => {
             let data = JSON.parse(rep.substr(47).slice(0, -2)).table.rows,
                 temp = '';
-
+                
             for (let i in data) {
                 if (data[i].c[0].v == '❌') continue;
-                temp += `<tr class="${title.id}" style="display: none;">`;
+                temp += `<tr class="${title.id}"${type == title ? '' : ' style="display: none;"'}>`;
                 for (let j in data[i].c) {
                     if (j == 2 && (data[i].c[3].v).indexOf('IG') != -1) {
                         temp += '<td>' + `<iframe src="https://www.instagram.com/${data[i].c[j]?.v}/embed" scrolling="no" frameborder="0"></iframe>` + '</td>';
@@ -28,23 +40,4 @@ const list = () => {
             document.querySelector('.shops').innerHTML = document.querySelector('.shops').innerHTML + temp;
         });
     });
-},
-    filter = (title, prev) => {
-        document.querySelectorAll(`.${title}`).forEach(x => {
-            x.removeAttribute('style');
-        });
-        if (prev) document.querySelectorAll(`.${prev}`).forEach(x => {
-            x.style.display = 'none';
-        });
-    };
-
-window.onload = () => {
-    document.querySelectorAll('option').forEach(x => {
-        x.innerHTML = x.id;
-        x.value = x.id;
-    });
-    list();
-    let type = localStorage.type || document.querySelector('#type > option').id;
-    filter(type);
-    document.querySelector(`#${type}`).selected = true;
 };
