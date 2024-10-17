1b174dec0a3dcc7ca06f3230752048a43806c00a
ace-yctko <ace.yctko@gmail.com>
2024-10-17 14:17:19 +0800

    u

--- src/shops-benefit.js
+++ src/shops-benefit.js
@@ -25,7 +25,6 @@ window.onload = () => {
     [titles[titles.indexOf(type)], titles[0]] = [titles[0], titles[titles.indexOf(type)]];
     
     titles.forEach(id => {
-        console.log(id)
         fetch('https://docs.google.com/spreadsheets/d/1gTkcU8G4240QNNcMdFWTwr4JKBBd1Qdjwpu8nE8gY7E/gviz/tq?sheet=' + id + '&range=A3:H')
         .then(res => res.text())
         .then(rep => {
